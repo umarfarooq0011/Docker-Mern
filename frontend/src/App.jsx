6d1d843b7cf74963e6e0c27eb476dc7bd6584a59
umar Awan <Umarfarooq6153@gmail.com>
2025-08-24 09:45:09 +0500

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,31 @@
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import Create from "./pages/Create";
+import Home from "./pages/Home";
+import "./App.css";
+
+function App() {
+  return (
+    <Router>
+      <nav className="navbar">
+        <Link to="/" className="navbar_title">
+          <h1>Anime</h1>
+        </Link>
+        <div className="navbar_links">
+          <Link to="/" className="navbar_link">
+            Home
+          </Link>
+          <Link to="/create" className="navbar_link">
+            Share
+          </Link>
+        </div>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/create" element={<Create />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
